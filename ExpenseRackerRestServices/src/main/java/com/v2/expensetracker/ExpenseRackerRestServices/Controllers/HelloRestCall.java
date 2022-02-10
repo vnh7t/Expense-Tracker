ec2b0188b95c439e50435d77e69437aa4f2bc793
vnh7t <varun.nelakuditi2020@gmail.com>
2022-02-09 21:22:30 -0600

    first rest call added

--- ExpenseRackerRestServices/src/main/java/com/v2/expensetracker/ExpenseRackerRestServices/Controllers/HelloRestCall.java
+++ ExpenseRackerRestServices/src/main/java/com/v2/expensetracker/ExpenseRackerRestServices/Controllers/HelloRestCall.java
@@ -1,2 +1,12 @@
-package com.v2.expensetracker.ExpenseRackerRestServices.Controllers;public class HelloRestCall {
+package com.v2.expensetracker.ExpenseRackerRestServices.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestCall {
+    @RequestMapping("/hello")
+    public String greetingMessage(){
+        return "Good Morning";
+    }
 }
