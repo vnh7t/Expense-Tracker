d53aa62e14276f4ec6e00d8de1f433e5adb1caed
vnh7t <varun.nelakuditi2020@gmail.com>
2022-02-12 20:47:33 -0600

    crud operations for single task changes

--- ExpenseRackerRestServices/src/main/java/com/v2/expensetracker/ExpenseRackerRestServices/Controllers/HelloRestCall.java
+++ ExpenseRackerRestServices/src/main/java/com/v2/expensetracker/ExpenseRackerRestServices/Controllers/HelloRestCall.java
@@ -1,12 +1,13 @@
 package com.v2.expensetracker.ExpenseRackerRestServices.Controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.Generated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloRestCall {
-    @RequestMapping("/hello")
+    @RequestMapping("/")
     public String greetingMessage(){
         return "Good Morning";
     }
+
 }
